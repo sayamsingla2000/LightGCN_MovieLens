ae041b3c6521b05c50f318c2df0245f74968ecbc
sayam singla <62406307+sayamsingla2000@users.noreply.github.com>
2021-09-06 17:43:35 +0530

    Update model.py

--- code/model.py
+++ code/model.py
@@ -1,13 +1,4 @@
-"""
-Created on Mar 1, 2020
-Pytorch Implementation of LightGCN in
-Xiangnan He et al. LightGCN: Simplifying and Powering Graph Convolution Network for Recommendation
 
-@author: Jianbai Ye (gusye@mail.ustc.edu.cn)
-
-Define models here
-
-"""
 
 """
 This file contains all the important function to build the model
